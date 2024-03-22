f20dc668aaac994c5333d5a8679c375af203e1da
fulk2014 <144195753+fulk2014@users.noreply.github.com>
2024-03-22 18:41:12 +0700

    ex03

---
+++ 0275/Lab09_Ex03/Program.cs
@@ -0,0 +1,27 @@
+﻿SecondDerivedClass sdRef  = new SecondDerivedClass();
+BaseClass bcRef = (BaseClass) sdRef;
+
+sdRef.Print();
+bcRef.Print();
+
+class BaseClass
+{
+    virtual public void Print()
+    {
+        System.Console.WriteLine("Hello from BaseClass");
+    }
+}
+class DerivedClass : BaseClass
+{
+    override public void Print()
+    {
+       System.Console.WriteLine("Hello from DerivedClass");
+    }
+}
+class SecondDerivedClass : DerivedClass
+{
+    override public void Print()
+    {
+       System.Console.WriteLine("Hello from SecondDerivedClass");
+    }
+}
