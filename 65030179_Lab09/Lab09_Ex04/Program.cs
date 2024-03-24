409c5a34749f9a93028238aadfcb5c51a59682df
PATTARAPON PONKUL <65030179@kmitl.ac.th>
2024-03-24 23:07:56 +0700

    commit Ex04

--- 65030179_Lab09/Lab09_Ex04/Program.cs
+++ 65030179_Lab09/Lab09_Ex04/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿SecondDerivedClass sdRef  = new SecondDerivedClass();
+BaseClass bcRef = (BaseClass) sdRef;
+
+sdRef.Print();
+bcRef.Print();
+
+class BaseClass
+{
+    virtual public void Print()
+    {
+        System.Console.WriteLine("Hello from BaseClass");
+    }
+}
+class DerivedClass : BaseClass
+{
+    override public void Print()
+    {
+       System.Console.WriteLine("Hello from DerivedClass");
+    }
+}
+class SecondDerivedClass : DerivedClass
+{
+    public void Print()
+    {
+       System.Console.WriteLine("Hello from SecondDerivedClass");
+    }
+}
