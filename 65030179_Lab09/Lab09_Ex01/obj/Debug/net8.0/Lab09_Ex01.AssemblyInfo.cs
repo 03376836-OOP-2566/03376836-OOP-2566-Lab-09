6e6a7f4fcf3286706bfef385860d394411edf17a
PATTARAPON PONKUL <65030179@kmitl.ac.th>
2024-03-24 22:48:49 +0700

    New Commit

--- 65030179_Lab09/Lab09_Ex01/obj/Debug/net8.0/Lab09_Ex01.AssemblyInfo.cs
+++ 65030179_Lab09/Lab09_Ex01/obj/Debug/net8.0/Lab09_Ex01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab09_Ex01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+184711d890ff2ae54857e78741c22a4a9ca53842")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab09_Ex01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab09_Ex01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
