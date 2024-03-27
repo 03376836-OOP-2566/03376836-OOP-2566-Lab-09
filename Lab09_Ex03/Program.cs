93327c57bd144180ad233b92b821d6c611394621
Khemmasedth <khmasade555@gmail.com>
2024-03-27 23:27:35 +0700

    finish

---
+++ Lab09_Ex03/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+SecondDerivedClass sdRef = new SecondDerivedClass();
+BaseClass bcRef = (BaseClass)sdRef;
+
+sdRef.Print();
+bcRef.Print();
+
+class BaseClass
+{
+    virtual public void Print()
+    {
+        System.Console.WriteLine("Hello from BaseClass");
+    }
+}
+class DerivedClass : BaseClass
+{
+    override public void Print()
+    {
+        System.Console.WriteLine("Hello from DerivedClass");
+    }
+}
+class SecondDerivedClass : DerivedClass
+{
+    override public void Print()
+    {
+        System.Console.WriteLine("Hello from SecondDerivedClass");
+    }
+}
