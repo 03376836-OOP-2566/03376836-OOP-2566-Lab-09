e156946e3ea1a0e257614f2520803787358e1aee
newyork <spidermanwin1@gmail.com>
2024-03-27 15:42:50 +0700

    ex1

---
+++ Lab09_Ex01/Program.cs
@@ -0,0 +1,21 @@
+﻿DerivedClass dcRef = new DerivedClass();
+BaseClass bcRef = new BaseClass();
+
+dcRef.Info();
+bcRef.Info();
+
+class BaseClass
+{
+    virtual public void Info()
+    {
+        System.Console.WriteLine("This is BaseClass");
+    }
+}
+
+class DerivedClass : BaseClass
+{
+    override public void Info()
+    {
+        System.Console.WriteLine("This is DerivedClass");
+    }
+}
